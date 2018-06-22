6d2e631d4634140e2a167e7735fca0612c5dab5f
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2018-06-22 14:10:28 +0300

    Enemy waves added, location changed

--- Fairy TD/Assets/Script/LevelManager.cs
+++ Fairy TD/Assets/Script/LevelManager.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
     public static LevelManager Instance;
 
+	public int wavesNumber = 3;
     public float gateStrength = 30f;
+
     public SimpleHealthBar healthBar;
     public Color mainColor;
     public Color secondColor;
 
     public CoinPanel coinPanel;
+	public Text wavesText;
 
     public Camera mainCamera;
 
-
+	private int currWave = 1;
     private int coins = 30;
     private float currStrength;
 
     public int Coins { get { return coins; } }
+	public int Waves { get { return wavesNumber; } }
+	public int CurrWave {
+		get { return currWave; } 
+		set { 
+			currWave = value;
+			//currWave = value%(wavesNumber+1); 
+			if(currWave <= wavesNumber)
+				UpdateWavesText ();
+		} 
+	}
+
+	private void UpdateWavesText()
+	{
+		wavesText.text = (currWave).ToString ("00") + "/" + wavesNumber.ToString ("00");
+	}
 
     void Awake()
     {
@@ -32,6 +51,7 @@ public class LevelManager : MonoBehaviour {
         currStrength = gateStrength;
         healthBar.UpdateColor(mainColor);
         healthBar.UpdateBar(currStrength, gateStrength);
+		UpdateWavesText ();
 	}
 	
 	// Update is called once per frame
--- Fairy TD/Assets/Script/SpawnPlace.cs
+++ Fairy TD/Assets/Script/SpawnPlace.cs
@@ -6,35 +6,49 @@ using UnityEngine;
 public class SpawnPlace : MonoBehaviour {
 
     public GameObject path;
-    public int monsterNumber = 1;
+	public float waveInterval = 5f;
+   // public int monsterNumber = 1;
     public float interval = 1f;
 
     public GameObject[] monsterPrefabs;
 
     private float timer = 0f;
+	private float monsterNumber = 3;
     private int spawnedNumber = 0;
 
+	private float waveTimer;
     private Transform[] pathPoints;
 
     // Use this for initialization
     void Start () {
         pathPoints = path.GetComponentsInChildren<Transform>();
         timer = interval;
-        Debug.Log("Pos " + transform.position);
+		waveTimer = waveInterval;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(spawnedNumber < monsterNumber)
-        {
-            if (timer <= 0)
-            {
-                GenerateObject();
-                timer = interval;
-            }
-            else
-                timer -= Time.deltaTime;       
-        }    
+		if (LevelManager.Instance.CurrWave <= LevelManager.Instance.Waves) 
+		{
+			
+			if (spawnedNumber < Math.Floor(monsterNumber)) {
+				if (timer <= 0) {
+					GenerateObject ();
+					timer = interval;
+				} else
+					timer -= Time.deltaTime;       
+			} else {
+				if (waveTimer > 0)
+					waveTimer -= Time.deltaTime;
+				else {
+					waveTimer = waveInterval;
+					spawnedNumber = 0;
+					LevelManager.Instance.CurrWave++;
+					monsterNumber += 1.5f;
+				}
+			}
+		}
+        
 	}
 
     private void GenerateObject()
